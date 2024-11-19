3649257e1a9f5a37e77561b751e72fb7a7155b2f
Thomas Jin <83102001+Bob1272001@users.noreply.github.com>
2024-11-19 16:35:33 -0500

    Updated Drive motors to TalonSRX - Isabella Shen

--- Team95-Reef-Repair/src/main/java/frc/robot/subsystems/Drive.java
+++ Team95-Reef-Repair/src/main/java/frc/robot/subsystems/Drive.java
@@ -2,12 +2,12 @@ package frc.robot.subsystems;
 
 
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.motorcontrol.PWMVictorSPX;
+import edu.wpi.first.wpilibj.motorcontrol.PWMTalonSRX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Drive extends SubsystemBase {
-    private PWMVictorSPX leftDriveMotor = new PWMVictorSPX(1); //change the number
-    private PWMVictorSPX rightDriveMotor = new PWMVictorSPX(2); //change the number
+    private PWMTalonSRX leftDriveMotor = new PWMTalonSRX(1); //change the number
+    private PWMTalonSRX rightDriveMotor = new PWMTalonSRX(2); //change the number
     private DifferentialDrive drive = new DifferentialDrive(leftDriveMotor,rightDriveMotor);
     public Drive() {
 
